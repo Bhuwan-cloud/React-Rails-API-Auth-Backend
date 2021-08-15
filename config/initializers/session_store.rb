bc063dc9565d7684cdcac11110d6add7a263f2a6
Bhuwan-cloud <bhuwanbhatt475@gmail.com>
2021-08-15 23:42:41 +0545

    ReactRailsAuthentication

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,9 @@
+if Rails.env == "production"
+
+Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "react-rails-auth.herokuapp.com"
+else
+    
+Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+
+end 
+
